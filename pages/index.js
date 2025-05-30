55ecd936fc6ae2518b9a34152f2826a29e4d54bf
Railton-AP-De-Paula-Web-designer <depaularailton91@gmail.com>
2025-05-30 16:06:04 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function home() {
-    return <h1>Jesus eu amo você. se você me ama da um sinal! ok</h1>
+  return <h1>Jesus eu amo você. se você me ama da um sinal! ok</h1>;
 }
 
 export default home;
